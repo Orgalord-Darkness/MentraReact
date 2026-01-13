703ac38268ef92ff360c2767e949072c77bc9a6d
Orgalord-Darkness <144434367+Orgalord-Darkness@users.noreply.github.com>
2026-01-13 21:20:10 +0100

    Afficahge questions + création scrit affichage réponses + utils random

--- src/components/QuizResults.tsx
+++ src/components/QuizResults.tsx
@@ -2,32 +2,53 @@ import {useState, useEffect} from 'react';
 import type Question from './../types/mentra-react.ts'
 
 export function QuizResults(){
+    let compteur = 0; 
     const url ='https://opentdb.com/api.php?amount=10&difficulty=easy';
-    const default_question: Question[] = [{
-        id: 1, 
-        type: 'test', 
-        difficulty: 'easy', 
-        category: 'Culture Général', 
-        question: 'ça va ?', 
-        correct_answer: 'oui',
-    }]
-    const [results, setResults] = useState<Question[]>(default_question); 
+    // const default_question: Question[] = [{
+    //     id: 1, 
+    //     type: 'test', 
+    //     difficulty: 'easy', 
+    //     category: 'Culture Général', 
+    //     question: 'ça va ?', 
+    //     correct_answer: 'oui',
+    // }]
+    const [results, setResults] = useState<Question[]>([]); 
     useEffect(()=> {
         fetch(url)
         .then(res => res.json())
         .then(data => {
-            setResults(data); 
+           setResults(
+            data.results.map((q: Question, index: number) => ({
+                ...q,
+                id: index + 1
+            }))
+            );
+ 
         })
     }, [])
 
     return(
     <>  
         <h2>Voici les résultats : </h2>
-        <p>{results.map(
+        {results.map(
             (result) =>(
-                result.question
+                compteur = compteur +1,
+                <section style={{ marginTop: '5px'}}> 
+                    <p>{compteur } {result.question}</p>
+                    <ul>
+                        {result.incorrect_answers.map(
+                            (answer) => (
+                                <li>. {answer}</li>
+                            )
+                            
+                        )}
+                        <li>{result.correct_answer}</li>
+                    </ul>
+                </section>
+                
             )
-        )}</p>
-    </>
+        )}
+        ${console.log(results)
+}    </>
     ); 
 }
--- src/types/mentra-react.ts
+++ src/types/mentra-react.ts
@@ -1,8 +1,14 @@
 export default interface Question {
     id?: number, 
-    type: string, 
-    difficulty: string, 
-    category: string, 
-    question: string, 
-    correct_answer: string,
+    // type: string, 
+    // difficulty: string, 
+    // category: string, 
+    // question: string, 
+    // correct_answer: string,
+    type: string; 
+    difficulty: string; 
+    category: string; 
+    question: string; 
+    correct_answer: string; 
+    incorrect_answers: string[];
 }
