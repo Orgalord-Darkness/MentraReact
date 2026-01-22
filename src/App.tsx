988ade96f5cbf077489e23b95053169122e6bc0f
Orgalord-Darkness <144434367+Orgalord-Darkness@users.noreply.github.com>
2026-01-22 23:14:06 +0100

    url fetch form

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import {QuizResults} from'./components/QuizResults.tsx'; 
-import InputDifficulty from'./components/InputDifficulty.tsx'; 
-import InputNbQuestions from'./components/InputNbQuestions.tsx'; 
-import InputCategories from'./components/InputCategories.tsx'; 
+// import {QuizResults} from'./components/QuizResults.tsx'; 
+// import InputDifficulty from'./components/InputDifficulty.tsx'; 
+// import InputNbQuestions from'./components/InputNbQuestions.tsx'; 
+// import InputCategories from'./components/InputCategories.tsx'; 
 import './App.css'
+import type { Category } from './types/mentra-react.ts';
+import urlFetch from'./components/UrlFetch.tsx';
+import AnswersList from './components/AnswersList.tsx';
 
 function App() {
+  const urlCategories ='https://opentdb.com/api_category.php';
+  const [categories, setCategories] = useState<Category[]>([]); 
 
+  const [difficulty, setDifficulty] = useState<string>('easy');
+  const [nbQuestions, setNbQuestions] = useState<number>(10);
+  const [category, setCategory] = useState<number>(9);  
+
+  const results = urlFetch(nbQuestions, difficulty, category);
+
+  useEffect(()=> {
+      fetch(urlCategories)
+      .then(res => res.json())
+      .then(data => {
+          setCategories(data.trivia_categories);
+      })
+  }, [])
+console.log('results : ', results);
   return (
     <>
       <div className='flex mx-auto'>
@@ -20,13 +39,49 @@ function App() {
         </a>
       </div>
       <h1 className="font-bold text-center mt-8">MentraReact</h1>
-      <InputCategories/>
+      <label>Difficulté : </label>
+      <select 
+        value={difficulty}
+        onChange={(e) => setDifficulty(e.target.value)}
+        className = 'border px-2 py-2'>
+          <option value="easy">Facile</option>
+          <option value="medium">Moyen</option>
+          <option value="hard">Difficile</option>
+      </select>
+      <label>Catégorie : </label>
+        <select 
+        value={category}
+        onChange={(e) => setCategory(Number(e.target.value))}
+        className = 'border px-2 py-2'>
+        {categories.map((category) =>( 
+            console.log(category), 
+            <option value={category.id}>{category.name}</option>
+        ),
+        )}
+        </select>
+        <label>Nombre de questions : </label>
+        <input type="number" 
+        value={nbQuestions} 
+        onChange={(e) => setNbQuestions(Number(e.target.value))} 
+        className="border"/>
+
+        <h2>Résultats : </h2>
+        {/* {console.log(results)} */}
+        <ul>
+          {results.map((question) => (
+            <div key={question.id}>
+              <li>{question.question}</li>
+              <AnswersList incorrectAnswers={question.incorrect_answers} goodAnswer={question.correct_answer}/>
+            </div>
+          ))}
+        </ul>
+      {/* <InputCategories/>
       <InputDifficulty/>
-      <InputNbQuestions/>
+      <InputNbQuestions/> */}
       {/* <InputCategories/> */}
-      <div className="card">
+      {/* <div className="card">
           <QuizResults/>
-      </div>
+      </div> */}
     </>
   )
 }
--- src/components/UrlFetch.tsx
+++ src/components/UrlFetch.tsx
@@ -2,21 +2,25 @@ import { useEffect, useState } from "react";
 import type { Question } from "../types/mentra-react";
 
 export default function urlFetch(nbQuestions:number, difficulty:string, category: number) {
-    const url = `https://opentdb.com/api.php?amount=${nbQuestions}&category=${category}&difficulty=${difficulty}`; 
     const [results, setResults] = useState<Question[]>([]); 
+ 
     useEffect(()=> {
+        if(!nbQuestions || !difficulty || !category){
+            return;
+        } 
+        const url = `https://opentdb.com/api.php?amount=${nbQuestions}&category=${category}&difficulty=${difficulty}`;
         fetch(url)
-        .then(res => res.json())
-        .then(data => {
-            setResults(
-            data.results.map((q: Question, index: number) => ({
-                ...q,
-                id: index + 1
-            }))
-            );
-    
-        })
-    }, [])
+            .then(res => res.json())
+            .then(data => {
+                setResults(
+                data.results.map((q: Question, index: number) => ({
+                    ...q,
+                    id: index + 1
+                }))
+                );
+        
+            })
+    }, [nbQuestions, difficulty, category])
 
     return results; 
 }
