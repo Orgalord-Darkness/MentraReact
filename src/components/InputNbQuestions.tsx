ef88244cf5f4ed8c20ab341f704b832e039a13c1
Orgalord-Darkness <144434367+Orgalord-Darkness@users.noreply.github.com>
2026-02-01 11:50:25 +0100

    cleanCode

--- src/components/InputNbQuestions.tsx
+++
@@ -1,8 +0,0 @@
-export default function InputNbQuestions() {
-    return(
-        <>
-            <label>Nombre de questions : </label>
-            <input type="number" className="border"/>
-        </>
-    )
-}
